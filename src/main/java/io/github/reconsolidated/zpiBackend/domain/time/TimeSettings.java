8b0c7252db18d875e3ee7f2613a4bca7fb8f17c0
ReConsolidated <gracjanpasik@gmail.com>
2023-09-26 12:55:50 +0200

    init

---
+++ src/main/java/io/github/reconsolidated/zpiBackend/domain/time/TimeSettings.java
@@ -0,0 +1,12 @@
+package io.github.reconsolidated.zpiBackend.domain.time;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class TimeSettings {
+    @Id
+    @GeneratedValue(generator = "time_settings_id_generator")
+    private Long id;
+}
