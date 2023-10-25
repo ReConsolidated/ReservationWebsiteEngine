6e88d6a3e876da420145477e3c1b3ced300ca400
szymi <shibek123@gamil.com>
2023-10-25 18:50:52 +0200

    Add Store class

---
+++ src/main/java/io/github/reconsolidated/zpibackend/features/store/Store.java
@@ -0,0 +1,29 @@
+package io.github.reconsolidated.zpibackend.features.store;
+
+import io.github.reconsolidated.zpibackend.features.item.Item;
+import io.github.reconsolidated.zpibackend.features.storeConfig.StoreConfig;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Store {
+
+    @Id
+    @GeneratedValue(generator = "store_generator")
+    private Long storeId;
+    @OneToOne
+    private StoreConfig storeConfig;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Item> items;
+
+
+}
