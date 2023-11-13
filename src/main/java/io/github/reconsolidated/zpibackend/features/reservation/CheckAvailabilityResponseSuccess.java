7bce655021d0f6a9b809d0aa1ba3f8479a772949
szymi <shibek123@gamil.com>
2023-11-13 18:47:35 +0100

    delete avability classes

--- src/main/java/io/github/reconsolidated/zpibackend/features/reservation/CheckAvailabilityResponseSuccess.java
+++
@@ -1,4 +0,0 @@
-package io.github.reconsolidated.zpibackend.features.reservation;
-
-public class CheckAvailabilityResponseSuccess {
-}
--- src/main/java/io/github/reconsolidated/zpibackend/features/reservation/Options.java
+++
@@ -1,4 +0,0 @@
-package io.github.reconsolidated.zpibackend.features.reservation;
-
-public record Options(Schedule schedule, Integer amount, Integer capacity) {
-}
