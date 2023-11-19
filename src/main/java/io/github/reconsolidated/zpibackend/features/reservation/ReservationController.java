eaa60eae98e46e32e61d2f0b7e38186c590018db
szymi <shibek123@gamil.com>
2023-11-19 20:16:59 +0100

    edd endpoint

---
+++ src/main/java/io/github/reconsolidated/zpibackend/features/reservation/ReservationController.java
@@ -0,0 +1,29 @@
+package io.github.reconsolidated.zpibackend.features.reservation;
+
+import io.github.reconsolidated.zpibackend.authentication.appUser.AppUser;
+import io.github.reconsolidated.zpibackend.authentication.currentUser.CurrentUser;
+import io.github.reconsolidated.zpibackend.features.store.dtos.CreateStoreDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/reservation", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @PostMapping
+    public ResponseEntity<?> reFetchSchedule(@CurrentUser AppUser currentUser, @RequestBody CheckAvailabilityRequest request) {
+
+        CheckAvailabilityResponse response = reservationService.checkAvailability(request);
+
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getResponseCode()));
+    }
+}
