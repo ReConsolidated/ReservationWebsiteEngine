20945d833b6c3dbee5714f4c611d9e36b9a6e631
Gracjan Pasik <Gracjan.Pasik@ringieraxelspringer.pl>
2023-11-22 18:22:58 +0100

    [ZPI-141] Add get reservations endpoint

--- src/main/java/io/github/reconsolidated/zpibackend/features/reservation/ReservationController.java
+++ src/main/java/io/github/reconsolidated/zpibackend/features/reservation/ReservationController.java
@@ -6,10 +6,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -24,5 +23,10 @@ public class ReservationController {
         CheckAvailabilityResponse response = reservationService.checkAvailability(request);
 
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getResponseCode()));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> userReservations(@CurrentUser AppUser currentUser) {
+        return ResponseEntity.ok(reservationService.getReservations(currentUser.getId()));
     }
 }
