6d0c38acc2c359af88a15b37c2d1b452b2dfa9c7
Gracjan Pasik <Gracjan.Pasik@ringieraxelspringer.pl>
2023-11-22 18:47:33 +0100

    [ZPI-145] change behaviour

--- src/main/java/io/github/reconsolidated/zpibackend/features/item/dtos/ItemDto.java
+++ src/main/java/io/github/reconsolidated/zpibackend/features/item/dtos/ItemDto.java
@@ -1,12 +1,16 @@
 package io.github.reconsolidated.zpibackend.features.item.dtos;
 
+import io.github.reconsolidated.zpibackend.features.availability.Availability;
 import io.github.reconsolidated.zpibackend.features.item.Item;
+import io.github.reconsolidated.zpibackend.features.item.SubItem;
 import io.github.reconsolidated.zpibackend.features.parameter.Parameter;
+import io.github.reconsolidated.zpibackend.features.reservation.ScheduleSlot;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -21,10 +25,15 @@ public class ItemDto {
     private String description;
     private String image;
     private List<Parameter> customAttributeList;
+    private List<SubItem> subItems;
+    private List<Availability> availabilities;
     private Integer capacity;
-    private ItemStatus itemStatus;
+    private Long availableAmount;
+    private Double mark;
+    private LocalDateTime earliestStart;
+    private LocalDateTime latestEnd;
 
-    public ItemDto(Item item, ItemStatus itemStatus) {
+    public ItemDto(Item item) {
         this.id = item.getItemId();
         this.active = item.getActive();
         this.title = item.getTitle();
@@ -33,6 +42,30 @@ public class ItemDto {
         this.image = item.getImage();
         this.customAttributeList = item.getCustomAttributeList();
         this.capacity = item.getAmount();
-        this.itemStatus = itemStatus;
+        this.availableAmount = Long.valueOf(item.getAmount());
+        this.mark = 3.0;
+        this.subItems = item.getSubItems();
+        this.availabilities = item.getSchedule().getAvailabilities();
+
+        for (ScheduleSlot slot : item.getSchedule().getAvailableScheduleSlots()) {
+            if (earliestStart == null ||
+                    isEarlier(slot.getStartDateTime(), earliestStart)) {
+                earliestStart = slot.getStartDateTime();
+            }
+            if (latestEnd == null ||
+                    isEarlier(latestEnd, slot.getEndDateTime())) {
+                latestEnd = slot.getEndDateTime();
+            }
+        }
+    }
+
+    private boolean isEarlier(LocalDateTime one, LocalDateTime other) {
+        if (one.getHour() < other.getHour()) {
+            return true;
+        }
+        if (one.getHour() > other.getHour()) {
+            return false;
+        }
+        return one.getMinute() < other.getMinute();
     }
 }
