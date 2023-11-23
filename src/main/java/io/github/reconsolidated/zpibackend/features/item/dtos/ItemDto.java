d98dce8da392f746b032e1c5c4ef4fb8509254ae
Gracjan Pasik <Gracjan.Pasik@ringieraxelspringer.pl>
2023-11-23 10:06:33 +0100

    [ZPI-141] fix

--- src/main/java/io/github/reconsolidated/zpibackend/features/item/dtos/ItemDto.java
+++ src/main/java/io/github/reconsolidated/zpibackend/features/item/dtos/ItemDto.java
@@ -48,7 +48,7 @@ public class ItemDto {
         this.availabilities = item.getSchedule().getAvailabilities();
 
         for (ScheduleSlot slot : item.getSchedule().getAvailableScheduleSlots()) {
-            if (earliestStartHour < slot.getStartDateTime().getHour()) {
+            if (earliestStartHour > slot.getStartDateTime().getHour()) {
                 earliestStartHour = slot.getStartDateTime().getHour();
             }
             if (latestEndHour < slot.getEndDateTime().getHour()) {
