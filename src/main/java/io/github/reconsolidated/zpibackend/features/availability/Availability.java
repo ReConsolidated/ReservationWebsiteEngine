65f4b608d22b4f91f06bc2295cdc6628992bdbce
szymi <shibek123@gamil.com>
2023-11-26 10:43:35 +0100

    use reservation type

--- src/main/java/io/github/reconsolidated/zpibackend/features/availability/Availability.java
+++ src/main/java/io/github/reconsolidated/zpibackend/features/availability/Availability.java
@@ -1,6 +1,7 @@
 package io.github.reconsolidated.zpibackend.features.availability;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.github.reconsolidated.zpibackend.features.reservation.ReservationType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,5 +18,5 @@ public class Availability {
     private LocalDateTime startDateTime;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime endDateTime;
-    private String type;
+    private ReservationType type;
 }
